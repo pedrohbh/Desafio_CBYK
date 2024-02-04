a7ca7b813a8c781356866d267a4e2c7ccc1fb763
Pedro Hoppe <pedrohbh@users.noreply.github.com>
2024-02-04 15:21:24 -0300

    Crição das tabelas Conta e Situação Conta - parte 2

--- src/main/resources/db/migration/V3__cria_tabela_conta.sql
+++ src/main/resources/db/migration/V3__cria_tabela_conta.sql
@@ -1,11 +1,12 @@
 create table desafio.conta
 (
-    id              bigserial primary key,
+    id              bigserial,
     data_vencimento date           not null,
     data_pagamento  date           not null,
     valor           numeric(19, 2) not null,
     descricao       varchar        not null,
-    situacao        varchar(100)   not null,
+    situacao_conta_id        smallint   not null,
     data_cadastro timestamp not null
 );
-
+alter table desafio.conta add constraint pk_conta primary key (id);
+alter table desafio.conta add constraint fk_conta_situacao_conta FOREIGN KEY (situacao_conta_id) references desafio.situacao_conta(id);
