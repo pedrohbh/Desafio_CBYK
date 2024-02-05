87beb4feaad4215de362733165336fa0614c9af0
Pedro Hoppe <pedrohbh@users.noreply.github.com>
2024-02-05 12:11:27 -0300

    Criação do sistema de segurança - parte 2

--- src/main/java/br/com/totvs/desafio_totvs/config/SecurityConfig.java
+++ src/main/java/br/com/totvs/desafio_totvs/config/SecurityConfig.java
@@ -2,9 +2,14 @@ package br.com.totvs.desafio_totvs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -14,13 +19,30 @@ public class SecurityConfig
 {
 
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity http) throws Exception
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
         return http.build();
+    }
+
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService()
+    {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
