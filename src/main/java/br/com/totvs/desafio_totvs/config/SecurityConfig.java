c15266330ef27cd3808cd121be57fb7e693a916a
Pedro Hoppe <pedrohbh@users.noreply.github.com>
2024-02-05 11:57:16 -0300

    Criação do sistema de segurança - parte 1

---
+++ src/main/java/br/com/totvs/desafio_totvs/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package br.com.totvs.desafio_totvs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig
+{
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception
+    {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+        return http.build();
+    }
+
+}
